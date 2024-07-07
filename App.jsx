46a41f5516ee249bd9c62b7b4e2be39a15196292
sitimaisaroh291001 <sitimaisaroh291001@gmail.com>
2024-07-07 08:40:12 +0700

    Mengedit App

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import {View, Text,} from "react-native";
+import AppNavigator from './src/AppNavigator';
 const App = () => {
-    return (
+    return AppNavigator(
         <View>My Kitchen</View>
     )
-}
+};
 export default App;
-

e8f382164645ac904da5741160a2f76223ca393a
sitimaisaroh291001 <sitimaisaroh291001@gmail.com>
2024-07-07 08:31:23 +0700

    menambah folder src

---
+++ src/AppNavigator.jsx
@@ -0,0 +1,21 @@
+// src/AppNavigator.js
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import RecipeScreen from './screens/RecipeScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Recipe" component={RecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
